80b3af022a428ac348162bb1537222995dac6094
Akpa001 <159956412+Akpa001@users.noreply.github.com>
2024-02-18 21:52:03 +0100

    Yes

---
+++ newfile.py
@@ -0,0 +1,18 @@
+playing_numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+wining_number = (2, 7, 9)
+print("Welcome to AJEX Number Guessing Game")
+
+print("Choose a number between 1 and 10 to win a prize")
+print(playing_numbers)
+player = int(input("Select a Winning Number: "))
+if player in wining_number:
+    print("Congratulatiins You Win")
+   
+else:
+    print("You Lose")
+    
+play_again = input("Do you want to play again?   (yes/no): ") 
+if :
+ 
+   
+    
