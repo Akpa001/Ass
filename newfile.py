03a38bd2d423657351aab2748ea54208a3a34c7d
Akpa001 <159956412+Akpa001@users.noreply.github.com>
2024-02-18 22:03:26 +0100

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,18 +1,12 @@
-playing_numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-wining_number = (2, 7, 9)
-print("Welcome to AJEX Number Guessing Game")
+print("USD and NGN converter ")
 
-print("Choose a number between 1 and 10 to win a prize")
-print(playing_numbers)
-player = int(input("Select a Winning Number: "))
-if player in wining_number:
-    print("Congratulatiins You Win")
-   
+Amount = int(input("Amount "))
+unit = input("NGN or USD ")
+
+if unit.upper() == "USD":
+	converted = Amount * 1515
+	print("Amount: " + str(converted))
 else:
-    print("You Lose")
-    
-play_again = input("Do you want to play again?   (yes/no): ") 
-if :
- 
-   
-    
+	converted = Amount / 1515
+	print("Amount: "+ str(converted))
+print("Done ")
